98496caaa656367f27a4e0ad85c0810ee6053316
shirase <shirase55@gmail.com>
2016-05-16 12:31:40 +0300

    Check array fields

--- ActiveForm.php
+++ ActiveForm.php
@@ -3,6 +3,7 @@
 namespace shirase\form;
 
 use Yii;
+use yii\db\ActiveQuery;
 
 /**
  * Class ActiveForm
@@ -12,14 +13,41 @@ class ActiveForm extends \kartik\widgets\ActiveForm {
 
     public function field($model, $attribute, $options = [])
     {
-        if (!$model->isAttributeSafe($attribute)) {
-            return Yii::createObject([
-                'class' => 'shirase\form\FakeActiveField',
-                'model' => $model,
-                'attribute' => $attribute,
-                'form' => $this,
-            ]);
+        $path = explode('[', str_replace(']', '', $attribute));
+
+        /**
+         * @var $last \yii\db\ActiveRecord
+         */
+        $last = null;
+        $prevAttr = null;
+        foreach($path as $attr) {
+            if ($last) {
+                $relation = $last->getRelation($prevAttr, false);
+                if ($relation instanceof ActiveQuery) {
+                    $modelClass = $relation->modelClass;
+                    $last = new $modelClass();
+                } else {
+                    $last = $last->{$prevAttr};
+                }
+            } else {
+                $last = $model;
+            }
+
+            if (!$last->isAttributeSafe($attr)) {
+                return Yii::createObject([
+                    'class' => 'shirase\form\FakeActiveField',
+                    'model' => $model,
+                    'attribute' => $attribute,
+                    'form' => $this,
+                ]);
+            }
+            $prevAttr = $attr;
         }
+
+        if (sizeof($path)>1 && !isset($options['labelOptions']['label'])) {
+            $options['labelOptions']['label'] = $last->getAttributeLabel($path[sizeof($path)-1]);
+        }
+
         return parent::field($model, $attribute, $options);
     }
 }
